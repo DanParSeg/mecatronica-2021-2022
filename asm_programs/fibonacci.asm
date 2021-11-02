7bb6e6a40dcdef99240daf9ea2b3c525296bf628
danparseg <daniel.parra.segovia@gmail.com>
2021-11-02 23:34:05 +0100

    mmmmmmmmmm

--- asm_programs/fibonacci.asm
+++ asm_programs/fibonacci.asm
@@ -1,12 +1,12 @@
-define :hola 0f
 
 #valor 1 en r3
+
 zero R0
 addi 1
 copy R0R3
 
-#valor 0 en R2
-zero R2
+#valor 1 en R2
+copy r0r2
 
 #r3 es el valor grande, lo paso temporalmente a r1 para sumar
 #r2 es el valor pequeño
---
+++ asm_programs/siguelineas.asm
@@ -0,0 +1,123 @@
+define :mask_sensors 3 #0011
+
+define :not_found_addr 0
+define :go_right_addr 1
+define :go_left_addr 2
+define :found_addr 3
+
+#pseudocodigo:
+#while(true){
+#   sensors=readSensors()
+#   if(sensors==0)
+#       not_found()
+#   else if(sensors==1)
+#       go_right()
+#   else if(sensors==2)
+#       go_left()
+#   else
+#       found()
+#}
+
+
+
+
+#podria guardar todas las direcciones de los tags en ram para no depender de addi en mitad del programa
+
+:loop
+    #cargo sensores en r0
+
+    copy r0r1
+    zero r0
+    addi :mask_sensors
+    copy r0r2
+    and
+    copy r0r2
+
+    #ahora tengo en r2 el estado de los sensores
+
+    frst
+    zero r1
+    #comparo con 00
+    cmp 
+
+    #cargo direccion de not_found
+        zero r0
+        addi :not_found_addr
+        copy r0r1
+        ld
+        copy r0r1
+        #Salto a :not_found
+        jump 2 #equal 
+    
+    frst
+    zero r0
+    addi 0001
+    copy r0r1
+    #comparo con 01
+    cmp 
+
+    #cargo direccion de go_right
+        zero r0
+        addi :go_right_addr
+        copy r0r1
+        ld
+        copy r0r1
+        #Salto a :go_right
+        jump 2 #equal 
+
+    frst
+    zero r0
+    addi 0010
+    copy r0r1
+    #comparo con 10
+    cmp 
+
+    #cargo direccion de go_left
+        zero r0
+        addi :go_left_addr
+        copy r0r1
+        ld
+        copy r0r1
+        #Salto a :go_left
+        jump 2 #equal 
+    
+    #else
+    #cargo direccion de found
+        zero r0
+        addi :found_addr
+        copy r0r1
+        ld
+        copy r0r1
+        #Salto a :found
+        jump 8 #always
+
+
+:not_found
+:go_right
+    zero r0
+    addi 0001
+
+    #return
+    zero r0
+    addi :loop
+    copy r0r1
+    jump 8
+:go_left
+    zero r0
+    addi 0010
+
+    #return
+    zero r0
+    addi :loop
+    copy r0r1
+    jump 8
+:found
+    zero r0
+    addi 0011
+    
+    #return
+    zero r0
+    addi :loop
+    copy r0r1
+    jump 8
+
